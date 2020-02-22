6e8dac4755b22bbf0c7bb64b7a2ac99fb1e8ec32
Bryan Schmidt <bryanschmidt1992@gmail.com>
2020-02-22 15:34:53 -0800

    Add main files to repo

---
+++ Source/PracticeProject/PracticeProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PracticeProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APracticeProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APracticeProjectGameMode();
+};
+
+
+
